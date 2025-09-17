dbb17ff5ac2f81db348a75e692cb3dad4a93a35b
Javid Mustafayev <dzavidmustafaev56@gmail.com>
2025-09-17 21:30:20 +0200

    added mockup

--- db.js
+++ db.js
@@ -4,11 +4,10 @@ const mysql=require('mysql2')
 const app=express()
 
 const conn=mysql.createConnection({
-  host: "sql7.freesqldatabase.com",
-  user: "sql7783287",
-  password: "E6sQkTN1nY",
-  database: "sql7783287",
-  port: 3306
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "guestbook",
 })
 
 conn.connect((err)=>{
